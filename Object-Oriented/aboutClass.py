32d72056ed50cbd74abdcd32c7edd5c88e0c2d9e
Matheus Oliveira <matheus-oliveira-batista@hotmail.com>
2023-09-26 20:53:31 +0100

    Aula muito boa
    Aprendi como funciona a parte de manipulacao basica, e consigui resolver um erro bobo sozinho em  meu codigo.

--- Object-Oriented/aboutClass.py
+++ Object-Oriented/aboutClass.py
@@ -3,4 +3,11 @@ from person import pessoa
 p1 = pessoa("Harry", 21) #Isso aqui eh a mesma coisa de criar variaveis como ( p1 = nome("Harry") e  p1 = idade(21) )
 p2 = pessoa("Potter", 32) #Como ja criamos uma classe em outro arquivo, eu estou colocando os valores na ordem que esta la)
 
-
+p1.comer("terra")
+p2.falar("carros")
+p1.falar("motos")
+p1.parar_comer()
+p2.comer("barro")
+p1.falar("moto")
+p1.falar("bikes")
+p1.falar("vida de gado")
--- Object-Oriented/person.py
+++ Object-Oriented/person.py
@@ -5,12 +5,39 @@ class pessoa:
         self.comendo  = comendo
         self.falando = falando
 
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f"{self.nome} nao pode falar pois esta COMENDO!")
+            return
+        
+        if self.falando:
+            print(f"{self.nome} ja esta FALANDO!")
+            return
+        
+        print(f"{self.nome} esta falando sobre {assunto}!")
+        self.falando = True
+
+
+    def parar_falar(self):
+        if not self.falando:
+            print(f"{self.nome} nao esta falando!")
+            return
+        
+        print(f"{self.nome} parou de falar!")
+        self.falando = False
+
+
     
     def comer(self, alimento): #Aqui agente vai pedir para a pessoa comer
         if self.comendo:
             print(f"Desculpe, mas {self.nome} ja esta comendo!")
             return 
-        #Nesse caso como o atributo la em cima  esta como "False" o Python ira ler o proximo comando aqui de baixo, se caso eu pedir novamente para ele comer ele vai ver que o estatus desse atributo agora estara como "True", entao e;ele printara essa mensagem "que ja esta comendo..."   
+        #Nesse caso como o atributo la em cima  esta como "False" o Python ira ler o proximo comando aqui de baixo, se caso eu pedir novamente para ele comer ele vai ver que o estatus desse atributo agora estara como "True", entao e;ele printara essa mensagem "que ja esta comendo..."  
+
+        if self.falando:
+            print(f"{self.nome} nao pode comer falando!")
+            return 
 
         print(f"{self.nome} esta comendo {alimento}")
         self.comendo = True #Como eu disse que ele esta comendo eu irei mudar o status "comendo"
